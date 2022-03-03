4ab83f670e3049c7ba07325ba96d9cb55186f547
rankiandras <rankiandras@gmail.com>
2022-03-03 15:08:04 +0100

    script.js modified

--- frontend/script.js
+++ frontend/script.js
@@ -1,74 +1,35 @@
-function Country(name, short, population, flag, continent) {
-    this.name = name;
-    this.short = short;
-    this.population = population;
-    this.flag = flag;
-    this.continent = continent;
-}
-
-
-const menuButton = () => {
-    return `
-    <button>
-        <svg width="40" heigth="40">
-            <rect width="20" height="2" />
-            <rect width="20" height="2" />
-            <rect width="20" height="2" />
-        </svg>
-        // <span>button</span>
-    </button>
-    `
-}
-
-
-// Components
-const header = (logo, button) => {
-    return `
-    <header>
-        <a id="logo">${logo}</a>
-        ${button()}
-    </header>
-    `
-}
-
-const countryCard = (name, short, population, flag, continent) => {
-    return `
-    <div ="card">
-        <h1>${name}</h1>
-        <h2>${short}</h2>
-        <p>${population}</p>
-        <img src="${flag}">
-        <p>${continent}</p>
-    </div>
-    `;
-}
-
-// menubutton
-
-const loadEvent = async _ => {
-    //Get Data    
-    const countryRes = await fetch("https://restcountries.com/v3.1/all");
-    const countryArr = await countryRes.json();
+function init(mathFunction) {
     
-    
-    //Process Data
-    let countries = countryArr.map(function(country) {
-        return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0]);
-    })
-    console.log(countries);
+    var a = 5;
+    var b = 6;
 
-    let cards = "";
-    for (const country of countries) {
-        cards += countryCard(country.name, country.short, country.population, country.flag, country.continent)       
-    }
-    const rootElement = document.getElementById("root")
-    rootElement.insertAdjacentHTML("beforeend", header("Countries", menuButton))
-    rootElement.insertAdjacentHTML("beforeend", cards)
+
+    if (a < b) {
+        let c = mathFunction(b, a);
+/*        (function() {
+           var c = b - a;
+           console.log(c);
+       })();*/
+        
+    }else {
+            let c = mathFunction(a, b);
+/*         (function() {
+            var c = a - b;
+        })();*/
+     };
+
+    // console.log(c);
+
+
 }
-// ez itt kurva gáz, valami van vele márc. 2. session felvételéről visszanézni!!
-    // const getMenuButton = document.getElementById
-    // getMenuButton.addEventListener("click", (event) => {
-    //     event.target.classList.toggle("clicked");
-    // });
 
-window.addEventListener("load", loadEvent);
+const initC = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber;
+};
+
+const initD = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+}
+
+init(initD);
+
